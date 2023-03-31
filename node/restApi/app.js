3e5823a9b4f5ccc4410e1a3c319156b204f0c7cb
Geethanjali-v <geethu9701@gmail.com>
2023-04-01 00:29:01 +0530

    mongo db added

--- node/restApi/app.js
+++ node/restApi/app.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongo =  require("mongodb");
+const MongoClient = mongo.MongoClient;
 const app = express();
+const PORT = 4001;
+
+
+const MONGO_URL = "mongodb://localhost:27017"
+
+// //Mongodb Connection
+// MongoClient.connect(MONGO_URL,(err,client) =>
+// console.log("Mongo is connected")
+// if (err)console.log("Error while connecting")
+
+// )
+
 const locations = [
           {
             "location_id": 1,
@@ -111,5 +125,14 @@ app.get("/", function (req, res) {
 app.get("/locations", function (req, res) {
     res.send(locations);
   });
+  //Mongodb Connection
+MongoClient.connect(MONGO_URL,(err,client) =>{
+console.log("Mongo is connected");
+if(err) console.log("Error while connecting");
+db = client.db("edureka");
 
-app.listen(4001);
+app.listen(PORT, () =>{
+console.log(`Server started on port ${PORT}`);
+});
+
+});
