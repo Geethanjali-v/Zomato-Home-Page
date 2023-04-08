65dfc1b05ecfe58061a807f1dc77c673f606ea0b
Geethanjali-v <geethu9701@gmail.com>
2023-04-09 01:07:17 +0530

    login added

--- node/loginApp/index.js
+++ node/loginApp/index.js
@@ -3,6 +3,7 @@ const app = express();
 const db = require("./db");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+
 app.use(cors());
 const AuthController = require("./controller/authController");
 app.use("/api/auth", AuthController)
