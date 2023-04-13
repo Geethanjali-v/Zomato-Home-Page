74ddfd9a6d84378a5e33b9244840e08373e1cbca
Geethanjali-v <geethu9701@gmail.com>
2023-04-13 22:56:20 +0530

    added react router

--- react/zomato-app/src/index.js
+++ react/zomato-app/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routing from './';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Routing/>
 );
 
 // If you want to start measuring performance in your app, pass a function
